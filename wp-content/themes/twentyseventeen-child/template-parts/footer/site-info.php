daff658f9711dd49100aaacfac06fc15a199c55e
Andrew E <vesper@vesper.local>
2017-07-18 20:53:14 -0700

    Create child theme from twentyseventeen called twentyseventeen-child with minimal design.

---
+++ wp-content/themes/twentyseventeen-child/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>">&copy; <?php bloginfo( 'name' ); ?>.</a>
+</div><!-- .site-info -->
