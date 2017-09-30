fcbe6db3e0177cae58147bdd6e84011be225a639
Andrew E <andrewevansc@gmail.com>
2017-09-29 22:34:30 -0700

    Create init function to limit images to only the sizes that already exist.

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -11,4 +11,21 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/*
+ * Creates only the exact sizes we want by overlapping with the other sizes that already exist.
+ */
+function remove__and_add_plugin_image_sizes() {
+
+    remove_image_size('twentyseventeen-featured-image');
+    add_image_size('twentyseventeen-featured-image', 1024, 1024);
+    remove_image_size('twentyseventeen-thumbnail-avatar');
+    add_image_size('twentyseventeen-thumbnail-avatar', 150, 150);
+
+    update_option('medium_large_size_w', '0');
+    update_option('medium_large_size_h', '0');
+}
+
+add_action('init', 'remove__and_add_plugin_image_sizes');
 ?>
+
