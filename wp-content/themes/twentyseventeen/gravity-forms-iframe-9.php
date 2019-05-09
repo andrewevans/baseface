c08f49f197cc39621e26ed84974170aced6a305b
Andrew E <andrewevansc@gmail.com>
2019-05-09 16:52:11 -0700

    Add duplicate Submit Best Offer form as ID 9 since that's what it is on prod.

---
+++ wp-content/themes/twentyseventeen/gravity-forms-iframe-9.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Our expertly curated collection of beautiful authentic fine art adorns the walls of collectors worldwide for over 20 years. From Rembrandt to Warhol, find your next acquisition.">
+    <meta name="robots" content="NOINDEX, NOFOLLOW">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="thYo8qV94rANW4wRVEoMcIrcIxOAeRz0gx6qh9Rf">
+    <link rel="icon" href="https://www.masterworksfineart.com/favicon-mfa.ico">
+
+    <title><?php echo esc_html( $form['title'] ); ?></title>
+
+    <script>
+      window.Laravel = {"csrfToken":"thYo8qV94rANW4wRVEoMcIrcIxOAeRz0gx6qh9Rf"};
+    </script>
+    <link rel="preload" href="/fonts/vendor/bootstrap-sass/bootstrap/glyphicons-halflings-regular.woff2" as="font">
+    <link href="//www.masterworksfineart.com/css/app.css" rel="stylesheet">
+</head>
+
+<body class="theme-butterfly">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PP8BTB2"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+<div class="navbar navbar-default" role="navigation" style="border: none; z-index: 1;">
+    <div class="container-fluid" style="border-bottom: 1px solid #ccd0d2;">
+        <div class="logo-xs visible-xs-block">
+            <a href="https://www.masterworksfineart.com">Masterworks Fine Art</a>
+        </div>
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- <a class="navbar-brand" href="http://gemini.andrew.com">Gemini</a>-->
+        </div>
+        <div id="navbar-main" class="navbar-collapse collapse" style="position: relative; margin-left: 0;">
+            <ul class="nav navbar-nav">
+                <li class="dropdown dropdown-large " style="opacity: 1.0">
+                    <a href="https://www.masterworksfineart.com/artists" >Artists</a>
+                </li>
+
+                <li><a href="//www.masterworksfineart.com/new-art-prints">New Arrivals</a></li>
+                <li><a href="https://www.masterworksfineart.com/contact-gallery/how-to-sell-fine-art-you-own">Selling</a></li>
+                <li><a href="https://www.masterworksfineart.com/about-us">About</a></li>
+                <li><a href="https://www.masterworksfineart.com/contact-gallery">Contact</a></li>
+                <li><a href="" style="color: #a00; font-weight: bold;"></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+        <div class="logo hidden-xs" style="display: inline; width: initial">
+            <a href="https://www.masterworksfineart.com" style="font-family: 'Helvetica', 'Arial', sans-serif;
+                letter-spacing: 0.03em;
+                color: #333 !important;
+                opacity: 1.0;
+                font-weight: 600;
+                text-decoration: none;
+                text-transform: uppercase;
+                font-size: 16px !important;"><img src="https://images.masterworksfineart.com/design/masterworks-fine-art-logo-sq.jpg" alt="Masterworks Fine Art Gallery" style="opacity: 0.8;">
+                Masterworks Fine Art</a>
+        </div>
+        <div class="contact-us-info">
+            <span><a href="https://shop.masterworksfineart.com/cart"><span class="size-l">Shopping Bag</span> <span class="glyphicon glyphicon-shopping-cart"></span> <span class="badge shop-cart-count"></span></a></span>
+            / <span class="phone-number">510-777-9970</span>
+            / <span id="phone_number_800">800-805-7060</span>
+            <a href="?design=old" style="color: #a00; font-weight: bold;">< Old design</a>
+        </div>
+    </div>
+
+</div><!-- will be used to show any messages -->
+<!-- will be used to show any messages -->
+
+<div class="container">
+    <?php GFFormDisplay::print_form_scripts( $form, false ); // ajax = false ?>
+    <?php gravity_form( $form_id, $display_title, $display_description ); ?>
+    <?php wp_footer(); ?>
+</div>
+
+<script>window.domain_url = 'www.masterworksfineart.com';</script>
+
+<div style="background: #000000; margin-top: 2em; margin-bottom: 1em; padding-top: 1em; padding-bottom: 1em">
+    <div class="container">
+        <footer>
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <p style="font-weight: bold !important">Explore the Fine Art Gallery</p>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.masterworksfineart.com">Home</a></li>
+                        <li><a href="https://www.masterworksfineart.com/artists">Artists Represented</a></li>
+                        <li><a href="https://www.masterworksfineart.com/fine-art-artists">Artists' Genres and Series</a></li>
+                        <li><a href="https://www.masterworksfineart.com/art-prints-for-sale">Art Prints For Sale</a></li>
+                        <li><a href="https://www.masterworksfineart.com/bio">Biographies</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us">About the Gallery</a></li>
+                        <li><a href="https://www.masterworksfineart.com/contact-gallery">Contact Us!</a></li>
+                        <li><a href="https://shop.masterworksfineart.com/cart">Shopping Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+                        <li><a href="https://www.masterworksfineart.com/contact-gallery/how-to-sell-fine-art-you-own">Sell Your Fine Art</a></li>
+                        <li><a href="https://news.masterworksfineart.com">Art News</a></li>
+                        <li><a href="https://www.masterworksfineart.com/educational-resources/">Tools for Art Collecting</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us/fine-art-gallery-careers">Careers</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us/press">Press</a></li>
+                        <li><a href="https://www.masterworksfineart.com/privacy-policy">Privacy Policy</a></li>
+                        <li><a href="https://www.masterworksfineart.com/login/facebook">Login</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <p style="font-weight: bold !important">Our Customer Service</p>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.masterworksfineart.com/about-us#always-there">Always there for you</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#certificate-of-authenticity">The Certificate of Authenticity</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#historical-docs">Historical Documentation</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#guanrantee">100% Moneyback Guarantee</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#framing">Museum-Archival Framing</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#pricing">Our Competitive Pricing</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#payments">Easy Payments</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#packing-insurance">Packaging and Insurance</a></li>
+                        <li><a href="https://www.masterworksfineart.com/about-us#appraisal">Free Annual Appraisal!</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <p style="font-weight: bold !important">Let's Get Social</p>
+                    <ul class="list-unstyled">
+                        <li style="display: inline-block"><a href="https://www.facebook.com/masterworksfineartgallery" target="_blank" rel="noopener"><img src="https://images.masterworksfineart.com/design/fb-icon.png" alt="Visit us on Facebook" /></a></li>
+                        <li style="display: inline-block"><a href="https://twitter.com/masterworksfa" target="_blank" rel="noopener"><img src="https://images.masterworksfineart.com/design/tw-icon.png" alt="Visit us on Twitter" /></a></li>
+                        <li style="display: inline-block"><a href="https://www.instagram.com/masterworksfineart/" target="_blank" rel="noopener"><img src="https://images.masterworksfineart.com/design/ig-icon.png" alt="Visit us on Instagram" /></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <p style="font-weight: bold !important">Contact Us</p>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://www.masterworksfineart.com/contact-gallery"><span class="glyphicon glyphicon-question-sign"></span> What do you think of our gallery?</a>
+                        </li>
+                        <li><a href="mailto:info@masterworksfineart.com">info@masterworksfineart.com</a></li>
+                        <li><div style="position: relative; display: block; padding: 10px 15px;"><a href="tel:510-777-9970" style="display: inline-block">510-777-9970</a> / <a href="tel:800-805-7060" style="display: inline-block">800-805-7060</a></div></li>
+                        <li><a href="https://goo.gl/maps/Pcq99SwRpbx" target="_blank" rel="noopener">Masterworks Fine Art Gallery<br />
+                                13470 Campus Drive<br />
+                                Oakland Hills, California USA 94619</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+
+<div class="container-fluid" style="text-align: center">
+    <p class="text-muted">
+        &copy; Masterworks Fine Art Gallery. All rights reserved. <a href="https://www.masterworksfineart.com/privacy-policy">Privacy Policy</a>. Our gallery is located in the beautiful Oakland Hills of the <b>San Francisco</b> Bay Area, California, USA.
+    </p>
+</div>
+
+<script src="//www.masterworksfineart.com/js/all.55ea53ceb1b63b2cbb7fed24f295e755.js"></script>
+<script src="//www.masterworksfineart.com/vendor/typeahead.0.11.1.bundle.min.js"></script>
+<script src="//www.masterworksfineart.com/js/search-typeahead.8bc32f702d1e54108060.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script', '//www.masterworksfineart.com/js/analytics.js','ga');
+
+  ga('create', 'UA-3116235-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+  var sc_project=3759295;
+  var sc_invisible=1;
+  var sc_security="ab715bd5";
+  var sc_https=1;
+  var scJsHost = (("https:" == document.location.protocol) ?
+    "https://secure." : "http://www.");
+</script>
+<script type="text/javascript"
+        src="https://www.statcounter.com/counter/counter.js"
+        async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - StatCounter" href="http://statcounter.com/"
+                                      target="_blank"><img class="statcounter"
+                                                           src="//c.statcounter.com/3759295/0/ab715bd5/1/" alt="Web
+Analytics Made Easy - StatCounter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+
+
+<style>
+    @media  print {
+
+        .v4-tease,
+        .navbar,
+        .breadcrumb,
+        #showcaser-hero,
+        .related-artworks,
+        .intro-header,
+        .filters,
+        footer {
+            display: none;
+        }
+
+        a[href]:after {
+            content: none !important;
+        }
+
+        .artworks {
+            width: 33% !important;
+            float: left;
+        }
+    }
+
+    @page  {
+        size: 8.5in 11in;
+    }
+</style>
+
+
+<?PHP
+$sender = 'rob@masterworksfineart.com';
+$recipient = 'rob@masterworksfineart.com';
+
+$subject = "Best Offer Form Has Been Viewed";
+$message = "This was the best offer page that was viewed: https://shop.masterworksfineart.com" . ($_SERVER && $_SERVER['REQUEST_URI'] ? $_SERVER['REQUEST_URI'] : '');
+$headers = 'From:' . $sender;
+
+wp_mail($recipient, $subject, $message, $headers);
+
+?>
+</body>
+</html>
